1523e17ba015f79192a0ce01a154ce0c1d6f7eaf
Joun <urbanscratcher@gmail.com>
2024-01-25 15:05:32 +0900

    arranged

--- sketch.js
+++ sketch.js
@@ -1,53 +1,57 @@
-//global for the controls and input 
+// [Global Variables] -----------------------------------
+// global for the controls and input
 var controls = null;
-//store visualisations in a container
+// store visualisations in a container
 var vis = null;
-//variable for the p5 sound object
+// variable for the p5 sound object
 var sound = null;
-//variable for p5 fast fourier transform
+// variable for p5 fast fourier transform
 var fourier;
 
-function preload(){
-	sound = loadSound('assets/stomper_reggae_bit.mp3');
+// [PRE-LOAD] -------------------------------------------
+function preload() {
+  sound = loadSound("assets/stomper_reggae_bit.mp3");
 }
 
-function setup(){
-	 createCanvas(windowWidth, windowHeight);
-	 background(0);
-	 controls = new ControlsAndInput();
+// [SETUP] ----------------------------------------------
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+  controls = new ControlsAndInput();
 
-	 //instantiate the fft object
-	 fourier = new p5.FFT();
+  //instantiate the fft object
+  fourier = new p5.FFT();
 
-	 //create a new visualisation container and add visualisations
-	 vis = new Visualisations();
-	 vis.add(new Spectrum());
-	 vis.add(new WavePattern());
-	 vis.add(new Needles());
-
+  //create a new visualisation container and add visualisations
+  vis = new Visualisations();
+  vis.add(new Spectrum());
+  vis.add(new WavePattern());
+  vis.add(new Needles());
 }
 
-function draw(){
-	background(0);
-	//draw the selected visualisation
-	vis.selectedVisual.draw();
-	//draw the controls on top.
-	controls.draw();
+// [DRAW] -----------------------------------------------
+function draw() {
+  background(0);
+  //draw the selected visualisation
+  vis.selectedVisual.draw();
+  //draw the controls on top.
+  controls.draw();
 }
 
-function mouseClicked(){
-	controls.mousePressed();
+// [GLOBAL EVENTS] ---------------------------------------
+function mouseClicked() {
+  controls.mousePressed();
 }
 
-function keyPressed(){
-	controls.keyPressed(keyCode);
+function keyPressed() {
+  controls.keyPressed(keyCode);
 }
 
-//when the window has been resized. Resize canvas to fit 
+//when the window has been resized. Resize canvas to fit
 //if the visualisation needs to be resized call its onResize method
-function windowResized(){
-	resizeCanvas(windowWidth, windowHeight);
-	if(vis.selectedVisual.hasOwnProperty('onResize')){
-		vis.selectedVisual.onResize();
-	}
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  if (vis.selectedVisual.hasOwnProperty("onResize")) {
+    vis.selectedVisual.onResize();
+  }
 }
